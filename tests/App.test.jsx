81ae6cb32749f3ed36c905bb7c7696c5c8d5c14d
Ariel Barrios <ariel29111978@hotmail.com>
2023-08-30 09:30:55 -0300

    Crea un primer snapshop de referencia

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,9 +1,11 @@
 import { render } from '@testing-library/react';
 import App from '../src/App';
 
-describe('<Pueba en <App / >', () => {
+describe('Pueba en <App />', () => {
   test('debe de hacer mach con el snapshop', () => {
     const title = 'Hola!';
-    render(<App title={title} />);
+    const { container } = render(<App title={title} />);
+
+    expect(container).toMatchSnapshot();
   });
 });
