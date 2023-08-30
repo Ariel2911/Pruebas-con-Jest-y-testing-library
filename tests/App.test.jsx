2e1f9d3f72354524baa91c90c2cb258ab05f885d
Ariel Barrios <ariel29111978@hotmail.com>
2023-08-30 09:51:46 -0300

    Agrega prueba: 'debe de mostrar el título en un h1'

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -8,4 +8,19 @@ describe('Pueba en <App />', () => {
 
     expect(container).toMatchSnapshot();
   });
+
+  test('debe de mostrar el título en un h1', () => {
+    const title = 'Hola!';
+    const { container, getByText } = render(<App title={title} />);
+
+    //comprueba que el título se encuentre en el componente
+    expect(getByText(title)).toBeTruthy();
+
+    //Comprueba que el h1 sea igual al título
+    const h1 = container.querySelector('h1');
+    expect(h1.innerHTML).toBe(title);
+
+    //Comprueba que el h1 contenga el título
+    expect(h1.innerHTML).toContain(title);
+  });
 });
