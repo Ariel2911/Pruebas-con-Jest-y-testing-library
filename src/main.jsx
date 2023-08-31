68fdbe8b8f727b7df1ea1796c77fead82dfd3b29
Ariel Barrios <ariel29111978@hotmail.com>
2023-08-31 15:49:36 -0300

    Agrega el componente CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { CounterApp } from './CounterApp.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App title="Hola!" />
+    {/* <App title="Hola!" /> */}
+    <CounterApp value={20} />
   </React.StrictMode>
 );
